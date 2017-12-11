f3580933c94cb45fca2345a3d0cbb9db598d5106
Unknown <Ariel042cohen@gmail.com>
2017-12-11 15:57:58 +0200

    Log all tickers to MongoDB

--- src/watcher.py
+++ src/watcher.py
@@ -1,48 +1,28 @@
-import os
 import json
 import requests
 import time
+import datetime
+from pymongo import MongoClient
 
 config = json.load(open('src/watcher_config.json'))
-markets_prices = {}
 markets = config['markets']
 sampling_time = 0
-
-
-def initialize_markets_prices_list():
-    for market in markets:
-        markets_prices[market['name']] = []
+client = MongoClient('mongodb://ariel:ariel@ds127536.mlab.com:27536/collector')
+db = client.collector
 
 
 def watch_markets():
-    os.makedirs('output', exist_ok=True)
-    initialize_markets_prices_list()
-    counter = 0
-
     while True:
-        request_time = time.time()
-        
         for market in markets:
             try:
                 res = requests.get(market['api']).json()
                 price = res[market['last_price_key']]
                 data = {
-                    'time': request_time,
+                    'date': datetime.datetime.utcnow(),
                     'price': float(price)
                 }
-                markets_prices[market['name']].append(data)
+
+                db[market['name']].insert_one(data)
             except:
                 continue
         time.sleep(config['sampling_time'])
-        counter += 1
-        sampling_time_minutes = config['sampling_time'] / 60
-
-        if config['file_update_time'] == sampling_time_minutes * counter:
-            update_file()
-            counter = 0
-
-
-def update_file():
-    for market_name in markets_prices.keys():
-        with open('output/' + market_name + '.json', 'w') as file:
-            json.dump(markets_prices[market_name], file)
